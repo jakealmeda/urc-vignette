f1b693f492deebb307c49591c6bd7de4439f8a66
Jake Almeda <jakealmeda@gmail.com>
2021-03-15 22:31:08 +0800

    1.0 | Also removed the extra div for debugging

--- urc-vignette.php
+++ urc-vignette.php
@@ -95,58 +95,49 @@ class URCVignetteJavaScripts {
 	// Subcribe form
 	private function urc_get_full_subscribe_form_viggy() {
 
-		/*if( function_exists( 'setup_original_subscribe' ) ) {
+		return '<div class="group mailchimp-subscribe">
+			<div class="item-subscribe">
+			<div class="pretitle"><span class="fontsize-xsml">For A</span> <span class="fontsize-sml">LIMITED TIME ONLY</span><br><span class="fontsize-xsml">Get </span> <span class="fontsize-sml">FREE</span> <span class="fontsize-xsml">Copies Of My</span></div>
+			<div class="photo"></div>
+			<div class="title hide-onmobile"><span class="fontsize-med">Enter Your Name &amp; Email Below for Instant Access:</span></div>
 
-			return setup_original_subscribe();
+				<!-- Begin Mailchimp Signup Form -->
+				<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+				<style type="text/css">
+				#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+				/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+				We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+				</style>
+				<div id="mc_embed_signup">
+				<form action="https://understandingrelationships.us2.list-manage.com/subscribe/post?u=8fcb0ea8d36b15793f40f7ee8&amp;id=0463614dd6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
+					<div id="mc_embed_signup_scroll">
 
-		} else {*/
-
-			return '<div class="group mailchimp-subscribe">
-				<div class="item-subscribe">
-				<div class="pretitle"><span class="fontsize-xsml">For A</span> <span class="fontsize-sml">LIMITED TIME ONLY</span><br><span class="fontsize-xsml">Get </span> <span class="fontsize-sml">FREE</span> <span class="fontsize-xsml">Copies Of My</span></div>
-				<div class="photo"></div>
-				<div class="title hide-onmobile"><span class="fontsize-med">Enter Your Name &amp; Email Below for Instant Access:</span></div>
-
-					<!-- Begin Mailchimp Signup Form -->
-					<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
-					<style type="text/css">
-					#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
-					/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
-					We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-					</style>
-					<div id="mc_embed_signup">
-					<form action="https://understandingrelationships.us2.list-manage.com/subscribe/post?u=8fcb0ea8d36b15793f40f7ee8&amp;id=0463614dd6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
-						<div id="mc_embed_signup_scroll">
-
-						<div class="indicates-required hide-onmobile"><span class="asterisk">*</span> indicates required</div>
-						<div class="mc-field-group">
-						<label for="mce-EMAIL">Email  <span class="asterisk">*</span>
-						</label>
-						<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-						</div>
-						<div class="mc-field-group">
-						<label for="mce-FNAME">Name   <span class="asterisk">*</span>
-						</label>
-						<input type="text" value="" name="FNAME" class="" id="mce-FNAME">
-						</div>
-						<div id="mce-responses" class="clear">
-						<div class="response" id="mce-error-response" style="display:none"></div>
-						<div class="response" id="mce-success-response" style="display:none"></div>
-						</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-						<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_8fcb0ea8d36b15793f40f7ee8_0463614dd6" tabindex="-1" value=""></div>
-						<div class="clear formsubmit"><input type="submit" value="Instant Access" name="subscribe" id="mc-embedded-subscribe" class="button" onclick="return gtag_report_conversion(\'https://understandingrelationships.com/members-section\')"></div>
-						</div>
-					</form>
+					<div class="indicates-required hide-onmobile"><span class="asterisk">*</span> indicates required</div>
+					<div class="mc-field-group">
+					<label for="mce-EMAIL">Email  <span class="asterisk">*</span>
+					</label>
+					<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
 					</div>
-
-					<!--End mc_embed_signup-->
-
-				<div class="disclaimer">Enter your name &amp; email in the boxes above to gain access to FREE Digital Online Versions of my popular eBooks &amp; audio course. When you click the “Instant Access” button, you will gain access to the members area of my website to read my eBooks, &amp; listen to the audio lessons right in your web browser!</div>
+					<div class="mc-field-group">
+					<label for="mce-FNAME">Name   <span class="asterisk">*</span>
+					</label>
+					<input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+					</div>
+					<div id="mce-responses" class="clear">
+					<div class="response" id="mce-error-response" style="display:none"></div>
+					<div class="response" id="mce-success-response" style="display:none"></div>
+					</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+					<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_8fcb0ea8d36b15793f40f7ee8_0463614dd6" tabindex="-1" value=""></div>
+					<div class="clear formsubmit"><input type="submit" value="Instant Access" name="subscribe" id="mc-embedded-subscribe" class="button" onclick="return gtag_report_conversion(\'https://understandingrelationships.com/members-section\')"></div>
+					</div>
+				</form>
 				</div>
-				</div>
-				<div><h1>HERE!</h1></div>';
 
-		//}
+				<!--End mc_embed_signup-->
+
+			<div class="disclaimer">Enter your name &amp; email in the boxes above to gain access to FREE Digital Online Versions of my popular eBooks &amp; audio course. When you click the “Instant Access” button, you will gain access to the members area of my website to read my eBooks, &amp; listen to the audio lessons right in your web browser!</div>
+			</div>
+			</div>';
 
 	}
 
